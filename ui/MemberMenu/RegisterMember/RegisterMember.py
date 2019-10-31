304ea126f7d086657938afab8b3e318bde727a53
Birna Valtýsdóttir <birnavaltys@gmail.com>
2019-10-31 13:58:03 +0000

    champion

---
+++ ui/MemberMenu/RegisterMember/RegisterMember.py
@@ -0,0 +1,12 @@
+class RegisterMember:
+    def __init__(self, manager):
+        self.manager = manager
+    
+    def register_member(self):
+        print('Please input member information\n')
+        name = input('Name: ')
+        phone = input('Phone: ')
+        email = input('Email: ')
+        year_of_birth = input('Year of birth: ')
+        #græja virknina bakvið return 
+        return 
---
+++ ui/MemberMenu/RegisterMember/RegisterMemberMenu.py
@@ -0,0 +1,19 @@
+import os 
+from colorama import Fore
+
+class RegisterMemberMenu:
+    def __init__(self, manager):
+        self.manager = manager
+    
+    def register_member_menu(self):
+        selection = ''
+        while selection != '9':
+            print('Register Member')
+            print('1. Register a member')
+            print('9. Go back')
+            selection = input()
+            os.system('cls')
+            if selection == '1':
+                self.manager.gotoClass('registermember')
+            elif selection == '9':
+                self.manager.gotoClass('sportmenu')
