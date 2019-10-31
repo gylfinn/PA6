e6b8660e9c737bae06fc5badc7634bfd51fec02b
Gylfi Helgason <gylfisuf@gmail.com>
2019-10-31 13:45:04 +0000

    Sett inn Remove Sport

---
+++ ui/SportMenu/RemoveSport/RemoveSport.py
@@ -0,0 +1,8 @@
+class RemoveSport:
+    def __init__(self,manager):
+        self.manager = manager
+
+    def removeSport(self):
+        print("Please Enter the name of the sport you want to remove: ")
+        rem = input()
+        #virkni
