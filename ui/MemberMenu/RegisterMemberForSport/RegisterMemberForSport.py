ed74835b8c45cb3a52ab82d6cc3689d1b444742a
Birna Valtýsdóttir <birnavaltys@gmail.com>
2019-11-01 09:17:20 +0000

    gamli vi skal ikke sprachen islanskur takktakk

--- ui/MemberMenu/RegisterMemberForSport/RegisterMemberForSport.py
+++ ui/MemberMenu/RegisterMemberForSport/RegisterMemberForSport.py
@@ -7,9 +7,9 @@ class RegisterMemberForSport:
     def registerMemberForSport(self):
         ans = ""
         print("Input the name of the athlete: ")
-        nafn = input()
+        name = input()
         os.system('cls')
-        if self.manager.model_members.find_member_by_name(nafn) == False:
+        if self.manager.model_members.find_member_by_name(name) == False:
             while ans != "9":
                 print("Athlete Not Found. Do you want to Register a new Member or try again?")
                 print("1. Register new Member")
@@ -25,15 +25,15 @@ class RegisterMemberForSport:
                     self.manager.gotoClass("membermenu")
         else:
             list_of_sports = self.manager.model_sports.get_all_sports()
-            teljari = 0
+            counter = 0
             for sport in list_of_sports:
-                teljari+=1
-                print("{}. {}".format(teljari,sport.name))
-            sport = input("Enter the name of the sport u want to register the member for: ")
+                counter+=1
+                print("{}. {}".format(counter,sport.name))
+            sport = input("Enter the name of the Sport you want to register member for: ")
             try:
-                self.manager.model_sports.add_member_to_sport(sport,nafn)
+                self.manager.model_sports.add_member_to_sport(sport,name)
                 os.system('cls')
-                print("{} successfully registered to {}\n".format(nafn,sport))
+                print("{} successfully registered to {}\n".format(name,sport))
             except KeyError:
                 os.system('cls')
                 print("You Entered the Incorrect Sport. Please Try again")
