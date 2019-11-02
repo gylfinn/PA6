8eda8bd39d30b867a6f95f2caeccd7fc9c87812f
Gylfi Helgason <gylfisuf@gmail.com>
2019-11-02 16:04:30 +0000

    FOXTROX UNIVERSE CHARLIE KILO

--- ui/MemberMenu/RemoveMember/RemoveFromSport.py
+++ ui/MemberMenu/RemoveMember/RemoveFromSport.py
@@ -9,32 +9,35 @@ class RemoveFromSport:
         name = input()
         member_info = self.manager.model_members.find_member_by_name(name)
         if member_info == False:
+            os.system('cls')
             print("Athlete Not Found, Please try again")
             self.try_again_go_back()
         else:
             sports = self.manager.model_sports.get_sports_where_member_is_reg(member_info)
             if len(sports) == 0:
-                print("{} is not registered for any sport".format(name))
+                os.system('cls')
+                print("{} is not registered for any sport\n".format(name))
                 self.try_again_go_back()
-                return
             else:
                 counter = 0
                 for sport in sports:
                     counter+=1
                     print("{}. {}".format(counter,sport.name))
                 remove_sport = input("Enter the name of the sport u want to remove from member: ")
+                os.system('cls')
                 try:
                     self.manager.model_sports.remove_athlete_from_sport(remove_sport,member_info)
-                    os.system('cls')
                     print("Athlete Successfully Removed from Sport\n")
                 except:
-                    pass
+                    print("Wrong Sport Entered.")
+                    self.try_again_go_back()
+
 
     def try_again_go_back(self):
         print("1. Try again")
         print("9. Go back")
         ans = input()
         if ans == "1":
-            self.manager.gotoClass("removememberforsport")
+            self.manager.gotoClass("removememberfromsport")
         elif ans == "9":
             self.manager.gotoClass("membermenu")
